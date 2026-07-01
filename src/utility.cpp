fb60a60b551405de539f2610c9dbc49df564cd52
piguyoverthere <panpanux1314@gmail.com>
2026-07-01 11:54:41 -0700

    i guess bro bro bro i guess bro i guess i guess

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,5 +1,7 @@
 #include "vex.h"
-void rotate(){
+void rotate(double target){
+    double error = (target - armRot.angle());
+    //pid controll thing later i wil write ok bye
     for (int i = 0; i > armDeg; i++){
         arm.spin(fwd,10,pct);
     }
