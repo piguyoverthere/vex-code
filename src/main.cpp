8136930b6bfe62a14bdcab2b44ed9419797dd107
piguyoverthere <panpanux1314@gmail.com>
2026-09-13 15:33:16 -0700

    commito

--- src/main.cpp
+++ src/main.cpp
@@ -122,8 +122,8 @@ void pre_auton() {
 
   while(!auto_started){
     Brain.Screen.clearScreen();
-    Brain.Screen.printAt(5, 20, "Liver v1.16.1");
-    Brain.Screen.printAt(5, 30, "# TEST VERSION by github/piguyoverthere");
+    Brain.Screen.printAt(5, 20, "Liver v1.18");
+    Brain.Screen.printAt(5, 30, "#K TEST VERSION by github/piguyoverthere");
     Brain.Screen.printAt(5, 40, "Battery Percentage:");
     Brain.Screen.printAt(5, 60, "%d", Brain.Battery.capacity());
     Brain.Screen.printAt(5, 80, "Chassis Heading Reading:");
@@ -288,7 +288,9 @@ void usercontrol(void) {
 
 
     if (master.ButtonY.pressing()){
-      toggleT();
+      toggle.spin(fwd,100,pct);
+    }else{
+      toggle.spin(fwd,0,pct);
     }
 
     if (master.ButtonR1.pressing()){
